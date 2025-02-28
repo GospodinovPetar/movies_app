97a212b21e2e5da60aae2a5c1e2ddb0990cbe4ec
Petar Gospodinov <leshokefa@Petars-MacBook-Pro.local>
2025-02-28 16:24:43 +0200

    Added drop down menu for the genres when adding a movie

--- movies_app/models.py
+++ movies_app/models.py
@@ -2,8 +2,29 @@
 from django.db import models
 
 class Movie(models.Model):
+    GENRE_CHOICES = [
+        ("Action", "Action"),
+        ("Adventure", "Adventure"),
+        ("Animation", "Animation"),
+        ("Comedy", "Comedy"),
+        ("Crime", "Crime"),
+        ("Documentary", "Documentary"),
+        ("Drama", "Drama"),
+        ("Family", "Family"),
+        ("Fantasy", "Fantasy"),
+        ("History", "History"),
+        ("Horror", "Horror"),
+        ("Music", "Music"),
+        ("Mystery", "Mystery"),
+        ("Romance", "Romance"),
+        ("Science Fiction", "Science Fiction"),
+        ("TV Movie", "TV Movie"),
+        ("Thriller", "Thriller"),
+        ("War", "War"),
+        ("Western", "Western"),
+    ]
     title = models.CharField(max_length=255)
-    genre = models.CharField(max_length=100) #TODO make a dropdown menu with the genres supported
+    genre = models.CharField(max_length=100, choices = GENRE_CHOICES) #TODO make a dropdown menu with the genres supported
     year = models.IntegerField()
     description = models.TextField()
 
